88841b4a02fc647b994eb52bac3c6c43f38bf9cf
atty303+hg.AT.gmail.com <none@none>
2008-04-05 22:19:21 +0900

    input/output reworks

--- tsniff/main.c
+++ tsniff/main.c
@@ -10,6 +10,10 @@
 #include "pseudo_bcas.h"
 #include "bcas_stream.h"
 
+#define TS_INPUT_FX2_PREFIX "fx2:"
+#define BCAS_INPUT_FX2_PREFIX "fx2:"
+#define BCAS_INPUT_PCSC_PREFIX "pcsc:"
+
 /* Options
    -------------------------------------------------------------------------- */
 //#define OPTION_FLAG_DEBUG G_OPTION_FLAG_HIDDEN
@@ -54,8 +58,6 @@ static gint st_b25_bcas_queue_size = 256;
 static gchar *st_b25_system_key = NULL;
 static gchar *st_b25_init_cbc = NULL;
 static GOptionEntry st_b25_options[] = {
-	{ "b25-enable", 'B', 0, G_OPTION_ARG_NONE, &st_b25_is_enabled,
-	  "Enable B25 decoder [disabled]", NULL },
 	{ "b25-round", 0, 0, G_OPTION_ARG_INT, &st_b25_round,
 	  "Set MULTI-2 round factor to N [4]", "N" },
 	{ "b25-strip", 'S', 0, G_OPTION_ARG_NONE, &st_b25_strip,
@@ -69,29 +71,47 @@ static GOptionEntry st_b25_options[] = {
 	{ NULL }
 };
 
-static gchar *st_ts_filename = NULL;
-static gchar *st_bcas_filename = NULL;
+static gchar *st_ts_input = BCAS_INPUT_FX2_PREFIX;
+static gchar *st_bcas_input = BCAS_INPUT_FX2_PREFIX;
+static gchar *st_ts_output = NULL;
+static gchar *st_bcas_output = NULL;
+static gchar *st_b25_output = NULL;
 static gboolean st_is_verbose = FALSE;
 static gboolean st_is_quiet = FALSE;
 static gint st_verify_bcas_stream = -1;
 static GOptionEntry st_main_options[] = {
-	{ "ts-filename", 't', 0, G_OPTION_ARG_FILENAME, &st_ts_filename,
-	  "Output MPEG2-TS to FILENAME", "FILENAME" },
-	{ "bcas-filename", 'b', 0, G_OPTION_ARG_FILENAME, &st_bcas_filename,
-	  "Output B-CAS stream to FILENAME", "FILENAME" },
+	{ "ts-input", 'T', 0, G_OPTION_ARG_FILENAME, &st_ts_input,
+	  "Input MPEG2-TS from SOURCE (" BCAS_INPUT_FX2_PREFIX " or FILENAME) [" BCAS_INPUT_FX2_PREFIX "]", "SOURCE" },
+	{ "bcas-input", 'B', 0, G_OPTION_ARG_FILENAME, &st_bcas_input,
+	  "Input B-CAS from SOURCE (" BCAS_INPUT_FX2_PREFIX " or " BCAS_INPUT_PCSC_PREFIX " or FILENAME) [" BCAS_INPUT_FX2_PREFIX ":]", "SOURCE" },
+
+	{ "ts-output", 't', 0, G_OPTION_ARG_FILENAME, &st_ts_output,
+	  "Output raw MPEG2-TS to FILENAME", "FILENAME" },
+	{ "bcas-output", 'b', 0, G_OPTION_ARG_FILENAME, &st_bcas_output,
+	  "Output B-CAS to FILENAME", "FILENAME" },
+	{ "b25-output", 'o', 0, G_OPTION_ARG_FILENAME, &st_b25_output,
+	  "Enable ARIB STD-B25 decoder and output to FILENAME", "FILENAME" },
+
 	{ "verbose", 'v', 0, G_OPTION_ARG_NONE, &st_is_verbose,
 	  "Verbose messages [disabled]", NULL },
 	{ "quiet", 'q', 0, G_OPTION_ARG_NONE, &st_is_quiet,
 	  "Quiet messages [disabled]", NULL },
+
 	{ "verify-bcas-stream", 0, OPTION_FLAG_DEBUG, G_OPTION_ARG_INT, &st_verify_bcas_stream,
-	  "Verify B-CAS stream CHUNK_SIZE=N", "N" },
+	  "Verify B-CAS stream CHUNK_SIZE=N (1-512)", "N" },
 	{ NULL }
 };
 
 
 static gboolean st_is_running = TRUE;
+static gboolean st_is_use_cusbfx2 = FALSE;
 static ARIB_STD_B25 *st_b25 = NULL;
 static B_CAS_CARD *st_bcas = NULL;
+static GIOChannel *st_ts_input_io = NULL;
+static GIOChannel *st_bcas_input_io = NULL;
+static GIOChannel *st_ts_output_io = NULL;
+static GIOChannel *st_bcas_output_io = NULL;
+static GIOChannel *st_b25_output_io = NULL;
 
 
 /* Ring buffer
@@ -139,201 +159,374 @@ restore_sighandler(void)
 
 /* Callbacks
    -------------------------------------------------------------------------- */
+static void
+proc_b25(gpointer data, gsize length)
+{
+	ARIB_STD_B25_BUFFER buffer;
+	gint r;
+
+	buffer.size = length;
+	buffer.data = data;
+	r = st_b25->put(st_b25, &buffer);
+	if (r < 0) {
+		g_critical("!!! b25->put %d", r);
+	}
+
+	r = st_b25->get(st_b25, &buffer);
+	if (r < 0) {
+		g_critical("!!! b25->get %d", r);
+	} else {
+		GError *error = NULL;
+		gsize written;
+		g_io_channel_write_chars(st_b25_output_io, buffer.data, buffer.size, &written, &error);
+		if (error) {
+			g_warning("[transfer_ts_callback] %s", error->message);
+			g_clear_error(&error);
+		}
+	}
+}
+
 static gboolean
 transfer_ts_cb(gpointer data, gint length, gpointer user_data)
 {
-	GIOChannel *io = (GIOChannel *)user_data;
 	GError *error = NULL;
 	gsize written;
 
-	if (st_b25) {
-		ARIB_STD_B25_BUFFER buffer;
+	if (st_ts_output_io) {
+		g_io_channel_write_chars(st_ts_output_io, data, length, &written, &error);
+		if (error) {
+			g_warning("[transfer_ts_callback] %s", error->message);
+			g_clear_error(&error);
+		}
+	}
+
+	if (st_b25_output_io) {
 		gint ret;
 		gpointer *chunk;
 
-		/* キューに積む */
-		chunk = g_slice_alloc(sizeof(gsize) + length);
-		*(gsize *)chunk = length;
-		memcpy(((gsize *)chunk) + 1, data, length);
-		g_queue_push_tail(st_b25_queue, chunk);
-		st_b25_queue_size += length;
+		if (st_b25_queue) {
+			/* キューに積む */
+			chunk = g_slice_alloc(sizeof(gsize) + length);
+			*(gsize *)chunk = length;
+			memcpy(((gsize *)chunk) + 1, data, length);
+			g_queue_push_tail(st_b25_queue, chunk);
+			st_b25_queue_size += length;
 
-		while (st_b25_queue_size > st_b25_queue_max) {
-			gpointer pop = g_queue_pop_head(st_b25_queue);
-			if (!pop) break;
+			while (st_b25_queue_size > st_b25_queue_max) {
+				gpointer pop = g_queue_pop_head(st_b25_queue);
+				gsize size = *(gsize *)pop;
 
-			buffer.size = *(gsize *)pop;
-			buffer.data = (gpointer)(((gsize *)pop) + 1);
-			ret = st_b25->put(st_b25, &buffer);
-			if (ret < 0) {
-				g_critical("b25->put %d", ret);
+				if (!pop) break;
+
+				proc_b25((gpointer)(((gsize *)pop) + 1), size);
+
+				g_slice_free1(sizeof(gsize) + size, pop);
+				st_b25_queue_size -= size;
 			}
-
-			g_slice_free1(sizeof(gsize) + buffer.size, pop);
-			st_b25_queue_size -= buffer.size;
-
-			ret = st_b25->get(st_b25, &buffer);
-			if (ret < 0) {
-				g_critical("b25->get %d", ret);
-			}
-			if (io) {
-				g_io_channel_write_chars(io, buffer.data, buffer.size, &written, &error);
-			}
+		} else {
+			proc_b25(data, length);
 		}
-	} else if (io) {
-		g_io_channel_write_chars(io, data, length, &written, &error);
 	}
-	return TRUE;
+
+	return st_is_running;
 }
 
 static gboolean
 transfer_bcas_cb(gpointer data, gint length, gpointer user_data)
 {
-	GIOChannel *io = (GIOChannel *)user_data;
-	GError *error = NULL;
-	gsize written;
-
 	if (st_bcas) {
 		((PSEUDO_B_CAS_CARD *)st_bcas)->push(st_bcas, data, length);
 	}
-	if (io) {
-		g_io_channel_write_chars(io, data, length, &written, &error);
+
+	if (st_bcas_output_io) {
+		gsize written;
+		GError *error = NULL;
+		g_io_channel_write_chars(st_bcas_output_io, data, length, &written, &error);
+		if (error) {
+			g_warning("[transfer_bcas_callback] %s", error->message);
+			g_clear_error(&error);
+		}
 	}
-	return TRUE;
+
+	return st_is_running;
 }
 
+
+static GIOChannel *
+open_io_channel(const gchar *filename, gboolean is_output)
+{
+	GIOChannel *io;
+	GError *error = NULL;
+
+	if (!strcmp(filename, "-")) {
+		io = g_io_channel_unix_new(is_output ? 1 : 0);
+	} else {
+		io = g_io_channel_new_file(filename, is_output ? "w" : "r", &error);
+	}
+
+	if (error) {
+		g_critical("[open_io_channel] %s", error->message);
+		g_clear_error(&error);
+		return NULL;
+	}
+
+	g_io_channel_set_encoding(io, NULL, &error);
+	if (error) {
+		g_critical("[open_io_channel] %s", error->message);
+		g_clear_error(&error);
+		g_io_channel_shutdown(io, FALSE, NULL);
+		return NULL;
+	}
+
+	return io;
+}
 
 static gboolean
 init_b25(void)
 {
-	g_message("Initializing B-CAS card reader emulator");
-	st_bcas = (B_CAS_CARD *)pseudo_bcas_new();
+	gboolean is_pseudo_bcas = TRUE;
+	gint r;
+
+	/* B-CAS カードクラスの初期化 */
+	if (g_str_has_prefix(st_bcas_input, BCAS_INPUT_PCSC_PREFIX)) {
+		g_message("*** using real B-CAS card reader");
+		st_bcas = create_b_cas_card();
+		is_pseudo_bcas = FALSE;
+	} else {
+		st_bcas = (B_CAS_CARD *)pseudo_bcas_new();
+
+		if (g_str_has_prefix(st_bcas_input, BCAS_INPUT_FX2_PREFIX)) {
+			g_message("*** using pseudo B-CAS card reader with CUSBFX2");
+		} else {
+			g_message("*** using pseudo B-CAS card reader with <%s>", st_bcas_input);
+		}
+	}
 	if (!st_bcas) {
-		g_critical("couldn't create B-CAS card reader");
-		return FALSE;
-	}
-	if (st_bcas->init(st_bcas)) {
-		g_critical("couldn't initialize B-CAS card reader");
+		g_critical("!!! couldn't create B-CAS card reader");
 		return FALSE;
 	}
 
-	g_message("Set B-CAS ECM buffer queue length to %d", st_b25_bcas_queue_size);
-	((PSEUDO_B_CAS_CARD *)st_bcas)->set_queue_len(st_bcas, st_b25_bcas_queue_size);
+	r = st_bcas->init(st_bcas);
+	if (r < 0) {
+		g_critical("!!! couldn't initialize B-CAS card reader (%d)", r);
+		return FALSE;
+	}
 
-	if (!((PSEUDO_B_CAS_CARD *)st_bcas)->set_init_status_from_hex(st_bcas, st_b25_system_key, st_b25_init_cbc)) {
-		g_critical("!!! B-CAS SYSTEM-KEY AND INIT-CBC NOT SUPPLIED !!!");
+	/* 仮想 B-CAS カードを使う場合の追加初期化 */
+	if (is_pseudo_bcas) {
+		if (!((PSEUDO_B_CAS_CARD *)st_bcas)->set_init_status_from_hex(st_bcas, st_b25_system_key, st_b25_init_cbc)) {
+			g_critical("!!! B-CAS SYSTEM KEY AND INIT-CBC NOT SUPPLIED");
+			st_bcas->release(st_bcas);
+			return FALSE;
+		}
+
+		if (g_str_has_prefix(st_bcas_input, BCAS_INPUT_FX2_PREFIX)) {
+			g_message("*** set B-CAS ECM buffer queue length to %d", st_b25_bcas_queue_size);
+			((PSEUDO_B_CAS_CARD *)st_bcas)->set_queue_len(st_bcas, st_b25_bcas_queue_size);
+		} else {
+			((PSEUDO_B_CAS_CARD *)st_bcas)->set_queue_len(st_bcas, G_MAXUINT);
+		}
+	}
+
+	g_message("*** initializing B25 decoder");
+	st_b25 = create_arib_std_b25();
+	if (!st_b25) {
+		g_critical("!!! couldn't create B25 decoder");
 		st_bcas->release(st_bcas);
 		return FALSE;
 	}
 
-	g_message("Initializing B25 decoder");
-	st_b25 = create_arib_std_b25();
-	if (!st_b25) {
-		g_critical("couldn't create B25 decoder");
-		return FALSE;
-	}
-
-	g_message("Set MULTI-2 round factor to %d", st_b25_round);
+	g_message("*** set MULTI-2 round factor to %d", st_b25_round);
 	st_b25->set_multi2_round(st_b25, st_b25_round);
 
-	g_message("%s omit NULL packets", st_b25_strip ? "Enable" : "Disable");
-	st_b25->set_strip(st_b25, 0);
+	g_message("*** %s omit NULL packets", st_b25_strip ? "Enable" : "Disable");
+	st_b25->set_strip(st_b25, st_b25_strip ? 1 : 0);
 
 	st_b25->set_b_cas_card(st_b25, st_bcas);
 
-	st_b25_queue = g_queue_new();
+	/* Initialize TS time-shift buffer */
+	if (g_str_has_prefix(st_bcas_input, BCAS_INPUT_FX2_PREFIX)) {
+		st_b25_queue = g_queue_new();
+		g_message("*** Initalize TS time-shift buffer");
+	}
 
 	return TRUE;
 }
 
 static void
-rec(void)
+release_b25(void)
+{
+	if (st_b25_queue) {
+		/* TODO: free chunks */
+		g_queue_free(st_b25_queue);
+	}
+	if (st_b25) st_b25->release(st_b25);
+	if (st_bcas) st_bcas->release(st_bcas);
+}
+
+static void
+run(void)
 {
 	cusbfx2_handle *device = NULL;
 	cusbfx2_transfer *transfer_ts = NULL;
 	cusbfx2_transfer *transfer_bcas = NULL;
-	GIOChannel *io_ts = NULL, *io_bcas = NULL;
 	GError *error = NULL;
 	GTimer *timer; 
+	gboolean is_cusbfx2_inited = FALSE;
+	gboolean is_cusbfx2_started = FALSE;
 
-	if (st_b25_is_enabled) {
-		if (!init_b25()) {
-			return;
+	/* Initialize inputs */
+	if (!g_str_has_prefix(st_ts_input, TS_INPUT_FX2_PREFIX)) {
+		if (!(st_ts_input_io = open_io_channel(st_ts_input, FALSE))) {
+			g_critical("!!! couldn't open TS input <%s>", st_ts_input);
+			goto quit;
 		}
+	}
+
+	if (!g_str_has_prefix(st_bcas_input, BCAS_INPUT_FX2_PREFIX) && !g_str_has_prefix(st_bcas_input, BCAS_INPUT_PCSC_PREFIX)) {
+		if (!(st_bcas_input_io = open_io_channel(st_bcas_input, FALSE))) {
+			g_critical("!!! couldn't open B-CAS input <%s>", st_bcas_input);
+			goto quit;
+		}
+	}
+
+	/* Initialize outputs */
+	if (st_ts_output) {
+		if (!(st_ts_output_io = open_io_channel(st_ts_output, TRUE))) {
+			g_critical("!!! couldn't open TS output <%s>", st_ts_output);
+			goto quit;
+		}
+	}
+	if (st_bcas_output) {
+		if (!(st_bcas_output_io = open_io_channel(st_bcas_output, TRUE))) {
+			g_critical("!!! couldn't open TS output <%s>", st_bcas_output);
+			goto quit;
+		}
+	}
+	if (st_b25_output) {
+		if (!(st_b25_output_io = open_io_channel(st_b25_output, TRUE))) {
+			g_critical("!!! couldn't open B25 output <%s>", st_b25_output);
+			goto quit;
+		}
+	}
+
+	/* Initialize B25 */
+	if (st_b25_output) {
+		if (!init_b25()) {
+			goto quit;
+		}
+	}
+
+	/* Initialize CUSBFX2 */
+	if (st_is_use_cusbfx2) {
+		cusbfx2_init();
+		is_cusbfx2_inited = TRUE;
+
+		device = capsts_open(st_fx2_id, st_fx2_is_force_load);
+		if (!device) {
+			goto quit;
+		}
+
+		capsts_cmd_push(CMD_PORT_CFG, 0x00, PIO_START|PIO_IR_OUT|PIO_TS_BACK);
+		capsts_cmd_push(CMD_MODE_IDLE);
+		capsts_cmd_push(CMD_IFCONFIG, 0xE3);
+		capsts_cmd_commit(device);
+
+		g_message("*** [capsts_set_ir_base] %d", st_ir_base);
+		capsts_set_ir_base(st_ir_base);
+		capsts_adjust_tuner_channel(device, st_ir_source, st_ir_channel, st_ir_three_channel);
+
+		if (g_str_has_prefix(st_ts_input, TS_INPUT_FX2_PREFIX)) {
+			g_message("*** setup TS transfer");
+			transfer_ts = cusbfx2_init_bulk_transfer(device, "TS", ENDPOINT_TS_IN,
+													 st_fx2_ts_buffer_size, st_fx2_ts_buffer_count,
+													 transfer_ts_cb, NULL);
+			if (!transfer_ts) {
+				g_critical("!!! couldn't setup TS transfer");
+				goto quit;
+			}
+			capsts_cmd_push(CMD_EP6IN_START);
+		}
+
+		if (g_str_has_prefix(st_bcas_input, BCAS_INPUT_FX2_PREFIX)) {
+			g_message("*** setup B-CAS transfer");
+			transfer_bcas = cusbfx2_init_bulk_transfer(device, "B-CAS", ENDPOINT_BCAS_IN,
+													   32, 1, transfer_bcas_cb, NULL);
+			if (!transfer_bcas) {
+				g_critical("!!! couldn't setup B-CAS transfer");
+				goto quit;
+			}
+			capsts_cmd_push(CMD_EP4IN_START);
+		}
+
+		capsts_cmd_push(CMD_PORT_WRITE, PIO_START|PIO_IR_OUT|PIO_TS_BACK);
+		capsts_cmd_commit(device);
+		is_cusbfx2_started = TRUE;
 	}
 
 	install_sighandler();
 
-	cusbfx2_init();
+	/* main loop */
+	/* B-CAS 入力がファイルであれば、事前に読んでおく */
+	if (st_bcas_input_io) {
+		for (;;) {
+			GError *error = NULL;
+			guint8 buf[512];
+			gsize readed;
 
-	g_message("capsts_set_ir_base: %d", st_ir_base);
-	capsts_set_ir_base(st_ir_base);
+			if (g_io_channel_read_chars(st_bcas_input_io, buf, 512, &readed, &error) != G_IO_STATUS_NORMAL) {
+				break;
+			}
+			if (error) {
+				g_warning("!!! %s", error->message);
+				g_clear_error(&error);
+			}
 
-	device = capsts_open(st_fx2_id, st_fx2_is_force_load);
-	if (!device) {
-		return;
-	}
+			if (st_bcas_output_io) {
+				gsize written;
+				g_io_channel_write_chars(st_bcas_output_io, buf, readed, &written, &error);
+				if (error) {
+					g_warning("!!! %s", error->message);
+					g_clear_error(&error);
+				}
+			}
 
-	capsts_cmd_push(CMD_PORT_CFG, 0x00, PIO_START|PIO_IR_OUT|PIO_TS_BACK);
-	capsts_cmd_push(CMD_MODE_IDLE);
-	capsts_cmd_push(CMD_IFCONFIG, 0xE3);
-	capsts_cmd_commit(device);
-
-	capsts_adjust_tuner_channel(device, st_ir_source, st_ir_channel, st_ir_three_channel);
-
-	if (st_ts_filename) {
-		io_ts = g_io_channel_new_file(st_ts_filename, "w", &error);
-		if (!io_ts) {
-			g_critical("Couldn't open TS output file %s", st_ts_filename);
-			goto quit;
+			if (st_bcas)
+				((PSEUDO_B_CAS_CARD *)st_bcas)->push(st_bcas, buf, readed);
 		}
-		g_io_channel_set_encoding(io_ts, NULL, &error);
-
-		g_message("Setup TS transfer");
-		transfer_ts = cusbfx2_init_bulk_transfer(device, "MPEG-TS", ENDPOINT_TS_IN,
-												 st_fx2_ts_buffer_size, st_fx2_ts_buffer_count,
-												 transfer_ts_cb, io_ts);
-		if (!transfer_ts) {
-			g_critical("Couldn't setup TS transfer");
-			goto quit;
-		}
-		capsts_cmd_push(CMD_EP6IN_START);
 	}
-
-	if (st_bcas_filename) {
-		io_bcas = g_io_channel_new_file(st_bcas_filename, "w", &error);
-		if (!io_bcas) {
-			g_critical("Couldn't open B-CAS output file %s", st_bcas_filename);
-			goto quit;
-		}
-		g_io_channel_set_encoding(io_bcas, NULL, &error);
-	}
-
-	if (st_bcas_filename || st_b25_is_enabled) {
-		g_message("Setup B-CAS transfer");
-		transfer_bcas = cusbfx2_init_bulk_transfer(device, "B-CAS", ENDPOINT_BCAS_IN,
-												   32, 1, transfer_bcas_cb, io_bcas);
-		if (!transfer_bcas) {
-			g_critical("Couldn't setup B-CAS transfer");
-			goto quit;
-		}
-		capsts_cmd_push(CMD_EP4IN_START);
-	}
-
-	capsts_cmd_push(CMD_PORT_WRITE, PIO_START|PIO_IR_OUT|PIO_TS_BACK);
-	capsts_cmd_commit(device);
 
 	timer = g_timer_new();
 	while (st_is_running) {
 		gdouble elapsed;
-		cusbfx2_poll();
-		elapsed = g_timer_elapsed(timer, NULL);
-		g_fprintf(stderr, "> Now:%.1f TS:%d(%3d%%)\r",
-				  elapsed,
-				  st_b25_queue_size,
-				  (gsize)((gdouble)st_b25_queue_size / st_b25_queue_max * 100));
-		if (elapsed > 180.0) {
-			break;
+
+		if (st_ts_input_io) {
+			GError *error = NULL;
+			guint8 buf[512];
+			gsize readed;
+			GIOStatus status;
+
+			status = g_io_channel_read_chars(st_ts_input_io, buf, 512, &readed, &error);
+			if (status == G_IO_STATUS_NORMAL) {
+				transfer_ts_cb(buf, 512, NULL);
+			} else {
+				if (error) {
+					g_warning("!!! %s", error->message);
+					g_clear_error(&error);
+				}
+				break;
+			}
+		} else if (is_cusbfx2_started) {
+			cusbfx2_poll();
+
+			elapsed = g_timer_elapsed(timer, NULL);
+			g_fprintf(stderr, ">>> Now:%.1f TS:%d(%3d%%)\r",
+					  elapsed,
+					  st_b25_queue_size,
+					  (gsize)((gdouble)st_b25_queue_size / st_b25_queue_max * 100));
+			if (elapsed > 180.0) {
+				break;
+			}
 		}
 	}
 	g_fprintf(stderr, "\n");
@@ -345,20 +538,34 @@ rec(void)
 		g_message("### INTERRUPTED SHUTDOWN ###");
 	}
 
-	if (st_ts_filename) capsts_cmd_push(CMD_EP6IN_STOP);
-	if (st_bcas_filename || st_b25_is_enabled) capsts_cmd_push(CMD_EP4IN_STOP);
-	capsts_cmd_push(CMD_MODE_IDLE);
-	capsts_cmd_commit(device);
-
  quit:
+	/* finalize */
 	restore_sighandler();
 
-	cusbfx2_free_transfer(transfer_ts);
-	cusbfx2_free_transfer(transfer_bcas);
-	if (io_ts) g_io_channel_close(io_ts);
-	if (io_bcas) g_io_channel_close(io_bcas);
-	cusbfx2_close(device);
-	cusbfx2_exit();
+	if (st_is_use_cusbfx2) {
+		if (is_cusbfx2_started) {
+			if (transfer_ts) capsts_cmd_push(CMD_EP6IN_STOP);
+			if (transfer_bcas) capsts_cmd_push(CMD_EP4IN_STOP);
+			capsts_cmd_push(CMD_MODE_IDLE);
+			capsts_cmd_commit(device);
+		}
+
+		/* TODO: poll? */
+
+		if (transfer_ts) cusbfx2_free_transfer(transfer_ts);
+		if (transfer_bcas) cusbfx2_free_transfer(transfer_bcas);
+
+		if (device) cusbfx2_close(device);
+		if (is_cusbfx2_inited) cusbfx2_exit();
+	}
+
+	release_b25();
+
+	if (st_b25_output_io) g_io_channel_shutdown(st_b25_output_io, TRUE, NULL);
+	if (st_bcas_output_io) g_io_channel_shutdown(st_bcas_output_io, TRUE, NULL);
+	if (st_ts_output_io) g_io_channel_shutdown(st_ts_output_io, TRUE, NULL);
+	if (st_bcas_input_io) g_io_channel_shutdown(st_bcas_input_io, TRUE, NULL);
+	if (st_ts_input_io) g_io_channel_shutdown(st_ts_input_io, TRUE, NULL);
 }
 
 /**
@@ -372,6 +579,7 @@ rec(void)
 static void
 verify_bcas_stream(void)
 {
+#if 0
 	guint8 *buf;
 	gsize readed;
 	GIOChannel *io_bcas = NULL;
@@ -397,6 +605,7 @@ verify_bcas_stream(void)
 	g_io_channel_close(io_bcas);
 	
 	return;
+#endif
 }
 
 static void
@@ -443,7 +652,7 @@ parse_options(int *argc, char ***argv)
 	g_option_group_add_entries(group_ir, st_ir_options);
 	g_option_context_add_group(context, group_ir);
 
-	group_b25 = g_option_group_new("b25", "ARIB STD-B25 Deocder Options:", "Show ARIB STD-B25 deocder options", NULL, NULL);
+	group_b25 = g_option_group_new("b25", "ARIB STD-B25 Decoder Options:", "Show ARIB STD-B25 decoder options", NULL, NULL);
 	g_option_group_add_entries(group_b25, st_b25_options);
 	g_option_context_add_group(context, group_b25);
 
@@ -454,6 +663,10 @@ parse_options(int *argc, char ***argv)
 	st_ir_base = CLAMP(st_ir_base, 1, 3);
 	st_ir_source = CLAMP(st_ir_source, -1, 2);
 	st_ir_channel = CLAMP(st_ir_channel, -1, 12);
+
+	if (g_str_has_prefix(st_ts_input, TS_INPUT_FX2_PREFIX) || g_str_has_prefix(st_bcas_input, TS_INPUT_FX2_PREFIX)) {
+		st_is_use_cusbfx2 = TRUE;
+	}
 
 	/* 3桁チャンネルのフォーマットチェック */
 	if (st_ir_three_channel) {
@@ -537,10 +750,10 @@ main(int argc, char **argv)
 		return 1;
 	}
 
-	if (st_verify_bcas_stream > 0 && st_bcas_filename) {
+	if (st_verify_bcas_stream > 0) {
 		verify_bcas_stream();
 	} else {
-		rec();
+		run();
 	}
 
 	return 0;
